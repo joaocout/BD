67930fab81480b7e446b610e4a6b25aa91c7eaf1
João Coutinho <39359575+joaocout@users.noreply.github.com>
2020-01-10 10:56:04 -0300

    Add files via upload

---
+++ atividade2.sql
@@ -0,0 +1,16 @@
+--https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
+
+--item 1
+SELECT CategoryName, Description FROM Categories WHERE CategoryID >=4 AND CategoryID <=6
+
+--item 2
+SELECT * FROM Shippers WHERE ShipperName = 'Speedy Express'
+
+--item 3
+SELECT * FROM Customers WHERE Country = 'Germany'
+
+--item 4
+SELECT CustomerName, Address, City, PostalCode FROM Customers WHERE Country = 'Brazil'
+
+--item 5
+
