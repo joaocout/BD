b414460e5c77c6ba6d5245142d2f118dfa61707f
João Coutinho <39359575+joaocout@users.noreply.github.com>
2020-01-14 10:08:50 -0300

    Add files via upload

--- atividade2.sql
+++ atividade2.sql
@@ -1,16 +1,37 @@
 --https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
 
 --item 1
-SELECT CategoryName, Description FROM Categories WHERE CategoryID >=4 AND CategoryID <=6
+SELECT CategoryName, Description FROM Categories WHERE CategoryID >=4 AND CategoryID <=6;
 
 --item 2
-SELECT * FROM Shippers WHERE ShipperName = 'Speedy Express'
+SELECT * FROM Shippers WHERE ShipperName = 'Speedy Express';
 
 --item 3
-SELECT * FROM Customers WHERE Country = 'Germany'
+SELECT * FROM Customers WHERE Country = 'Germany';
 
 --item 4
-SELECT CustomerName, Address, City, PostalCode FROM Customers WHERE Country = 'Brazil'
+SELECT CustomerName, Address, City, PostalCode FROM Customers WHERE Country = 'Brazil';
 
 --item 5
+SELECT Categories.Description, Suppliers.SupplierName, Products.ProductName, Products.Price
+FROM ((Products
+INNER JOIN Categories ON Categories.CategoryID = Products.CategoryID)
+INNER JOIN Suppliers ON Suppliers.SupplierID = Products.SupplierID);
 
+--item 6
+SELECT DISTINCT Price FROM Products;
+
+--item 7
+SELECT DISTINCT Country FROM Suppliers;
+
+--item 8
+SELECT OrderID FROM Orders WHERE CustomerID = 55 AND EmployeeID = 4;
+
+--item 9
+SELECT Orders.OrderID, Products.ProductName
+FROM ((Orders
+INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID)
+INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID);
+
+--item 10
+SELECT Price FROM Products ORDER BY Price DESC;
